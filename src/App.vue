2bf2b3d9fa427d0abff6f45de4307405164ae01e
“Jhalaa” <jhalaachinoy@gmail.com>
2018-03-05 10:33:19 -0800

    making changes to make api call

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      apiResponse: {}
+    }
+  },
+
+  mounted () {
+    axios({
+      method: 'GET',
+      auth: {
+        // need to mask credentials later or encrypt file
+        username: 'KUQ02CQUTTTF53WM813M',
+        password: 'pSroA51xnkAzelsQMizDXhjwvKnWM0p8'
+      },
+      params: {
+        address: '483+Bright+St',
+        zipcode: 94132
+      },
+      url: 'https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/property/details'
+    })
+      .then(response => {
+        this.apiResponse = response.data[0]['property/details'].result
+      })
+      .catch(error => {
+        this.apiResponse = error.response
+      })
+  }
 }
 </script>
 
