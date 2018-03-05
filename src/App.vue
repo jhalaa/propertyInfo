f0625823ee986f19d86d3b1bc3a5311756e8700b
“Jhalaa” <jhalaachinoy@gmail.com>
2018-03-05 11:06:14 -0800

    Changing app component and moving things to results

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,8 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
-  name: 'App',
-  data () {
-    return {
-      apiResponse: {}
-    }
-  },
-
-  mounted () {
-    axios({
-      method: 'GET',
-      auth: {
-        // need to mask credentials later or encrypt file
-        username: 'KUQ02CQUTTTF53WM813M',
-        password: 'pSroA51xnkAzelsQMizDXhjwvKnWM0p8'
-      },
-      params: {
-        address: '483+Bright+St',
-        zipcode: 94132
-      },
-      url: 'https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/property/details'
-    })
-      .then(response => {
-        this.apiResponse = response.data[0]['property/details'].result
-      })
-      .catch(error => {
-        this.apiResponse = error.response
-      })
-  }
+  name: 'App'
 }
 </script>
 
