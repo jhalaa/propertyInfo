8e1003fde33865e8442a9a9162eac7d8175ed5ad
“Jhalaa” <jhalaachinoy@gmail.com>
2018-03-05 10:02:37 -0800

    Adding footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="footer navbar-fixed-bottom">
+    <div class="row-md">
+      <div class="footer-text col-md-2 col-md-offset-2">About</div>
+      <div class="footer-text col-md-2 col-md-offset-2">Contact Us</div>
+      <div class="footer-text col-md-2 col-md-offset-2">Social Media</div>
+    </div>
+  </footer>
+</template>
+
+<script type="text/javascript">
+export default {
+
+}
+</script>
+
+<style>
+footer{
+  height: 40px;
+  background-color: rgb(0,219,182);
+}
+.footer-text{
+  padding: 10px;
+  font-size: 15px;
+  color: rgba(255, 0, 0, 0.5);
+}
+</style>
---
+++ test/unit/specs/Footer.spec.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Footer from '@/components/Footer'
+
+describe('Footer.vue', () => {
+  it('should have a fixed footer', () => {
+    const Constructor = Vue.extend(Footer)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.className)
+      .to.equal('footer navbar-fixed-bottom')
+  })
+
+  it('should have 3 sections', () => {
+    const Constructor = Vue.extend(Footer)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.children[0].querySelectorAll('div').length)
+      .to.equal(3)
+  })
+
+  it('should have first section about', () => {
+    const Constructor = Vue.extend(Footer)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.children[0].querySelectorAll('div')[0].textContent)
+      .to.equal('About')
+  })
+
+  it('should have second section contact us', () => {
+    const Constructor = Vue.extend(Footer)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.children[0].querySelectorAll('div')[1].textContent)
+      .to.equal('Contact Us')
+  })
+
+  it('should have third section social media', () => {
+    const Constructor = Vue.extend(Footer)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.children[0].querySelectorAll('div')[2].textContent)
+      .to.equal('Social Media')
+  })
+})
